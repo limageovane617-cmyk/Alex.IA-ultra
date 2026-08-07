b6502efaafdc96a23ee1e379bfac531e2f77e849
limageovane617-cmyk <limageovane617@gmail.com>
2026-08-07 18:42:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -65,11 +65,6 @@ if api_key:
         # Barra lateral
         st.sidebar.header("⚙️ Ferramentas")
 
-        modo = st.sidebar.radio(
-            "Escolha o modo:",
-            ["🤖 IA normal", "🎭 Personagem"]
-        )
-
         if st.sidebar.button("🗑️ Limpar conversa"):
             st.session_state.mensagens = []
             st.rerun()
