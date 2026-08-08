a96274422e4ba2f64ea0bff169ae5ae529cd89ae
limageovane617-cmyk <limageovane617@gmail.com>
2026-08-08 18:42:18 -0300

    Update __init__.py

--- video/motores/__init__.py
+++ video/motores/__init__.py
@@ -1,1 +1,6 @@
+# ============================================================
+# 🎬 ALEX IA ULTRA — MOTORES DE VÍDEO
+# Criada por Geovani
+# ============================================================
 
+# Este arquivo transforma "motores" em um módulo Python.
