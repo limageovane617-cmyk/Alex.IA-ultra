18b51926670775a0a0c5ac768e2d954972f8521f
limageovane617-cmyk <limageovane617@gmail.com>
2026-08-22 13:10:57 -0300

    Update test.sh

--- MobileI2V-Teste/test.sh
+++ MobileI2V-Teste/test.sh
@@ -1,1 +1,7 @@
-
+CUDA_VISIBLE_DEVICES=0 python scripts/inference_i2v.py \
+      --config=./configs/mobilei2v_config/MobileI2V_300M_img512.yaml \
+      --save_path=humface_1126 \
+      --model_path=./model/hybrid_371.pth \
+      --txt_file=asset/test.txt \
+      --flow_score=2.0 \
+      --prompt=""
